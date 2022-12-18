1a6a97c717bd0b63196dfd3c671cf44948a0ba30
CosmoboyMe <2014failz2014@gmail.com>
2022-12-18 13:57:54 +0100

    feat: create routing

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,6 @@ import './App.css'
 import { Header } from './components/Header'
 import { AppRoutes } from './routes'
 
-import { Header } from './components/Header'
-
 function App() {
   return (
     <div className='app'>
