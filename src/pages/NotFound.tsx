fc0892128d608a2361cf98b09ca1d212dddc9a85
CosmoboyMe <2014failz2014@gmail.com>
2022-12-18 13:56:39 +0100

    feat: create NotFound page

---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,37 @@
+import styled from 'styled-components'
+import { FC } from 'react'
+
+const Wrapper = styled.div`
+  max-width: 1110px;
+  padding: 0 20px;
+  margin: 195px auto 228px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  row-gap: 62px;
+`
+
+const Text = styled.p`
+  font-size: 165px;
+  line-height: 165px;
+  font-weight: 100;
+`
+
+const Title = styled.h1`
+  font-size: 165px;
+  line-height: 165px;
+  font-weight: 700;
+  text-transform: uppercase;
+`
+
+const NotFound: FC = () => {
+  return (
+    <Wrapper>
+      <Text>(404)</Text>
+      <Title>Not found</Title>
+    </Wrapper>
+  )
+}
+
+export { NotFound }
