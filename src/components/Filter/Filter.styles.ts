c5b82850e3f64f0ea6f7c727641f25b15f95b84e
CosmoboyMe <2014failz2014@gmail.com>
2022-12-30 21:12:07 +0100

    feat: create filter

---
+++ src/components/Filter/Filter.styles.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+
+const Filter = styled.div`
+  display: flex;
+  flex-direction: column;
+  row-gap: 40px;
+  max-width: 300px;
+  width: 100%;
+`
+
+const FilterCheckboxList = styled.div`
+  display: flex;
+  flex-direction: column;
+  max-height: 310px;
+`
+
+export { Filter, FilterCheckboxList }
