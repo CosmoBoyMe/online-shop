69d8dbb00daa4df6287b382a4487b258f0a83be4
yegorkosyak <yegorkosyak32@gmail.com>
2022-12-30 20:34:11 +0100

    refactor: move header to app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css'
+import { Header } from './components/Header'
 import { AppRoutes } from './routes'
 
 function App() {
   return (
     <div className='app'>
+      <Header />
       <AppRoutes />
     </div>
   )
