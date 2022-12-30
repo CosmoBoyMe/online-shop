aa63f283fe22dab452732fb550f5eabff2b82cbf
CosmoboyMe <2014failz2014@gmail.com>
2022-12-30 21:10:26 +0100

    feat: create dualSlider

---
+++ src/components/DualSlider/index.tsx
@@ -0,0 +1,71 @@
+import { useState, useEffect, useRef, ChangeEvent, memo } from 'react'
+
+import * as S from './DualSlider.styles'
+
+interface IDualSliderProps {
+  readonly min: number
+  readonly max: number
+  readonly title: string
+  readonly mark?: string
+  onChange: ({ min, max }: { min: number; max: number }) => void
+}
+
+const DualSlider = memo(function DualSlider({ min, max, title, mark, onChange }: IDualSliderProps) {
+  const [minValue, setMinValue] = useState(min)
+  const [maxValue, setMaxValue] = useState(max)
+  const minValueRef = useRef<HTMLInputElement>(null)
+  const maxValueRef = useRef<HTMLInputElement>(null)
+
+  const handleChangeMinThumb = (event: ChangeEvent<HTMLInputElement>) => {
+    const value = Math.min(Number(event.target.value), maxValue - 1)
+    setMinValue(value)
+    event.target.value = value.toString()
+  }
+
+  const handleChangeMaxThumb = (event: ChangeEvent<HTMLInputElement>) => {
+    const value = Math.max(Number(event.target.value), minValue + 1)
+    setMaxValue(value)
+    event.target.value = value.toString()
+  }
+
+  useEffect(() => {
+    onChange({ min: minValue, max: maxValue })
+  }, [minValue, maxValue, onChange])
+
+  return (
+    <S.DualSLider>
+      <S.Title>{title}</S.Title>
+      <S.Content>
+        <S.LeftThumb
+          type='range'
+          min={min}
+          max={max}
+          value={minValue}
+          ref={minValueRef}
+          onChange={handleChangeMinThumb}
+        />
+        <S.RightThumb
+          type='range'
+          min={min}
+          max={max}
+          value={maxValue}
+          ref={maxValueRef}
+          onChange={handleChangeMaxThumb}
+        />
+        <S.Track />
+        <S.Fields>
+          <S.ValueField>
+            {minValue}
+            <S.MarkSymbol>{mark}</S.MarkSymbol>
+          </S.ValueField>
+          <S.ValueField>
+            {maxValue}
+            <S.MarkSymbol>{mark}</S.MarkSymbol>
+          </S.ValueField>
+        </S.Fields>
+      </S.Content>
+    </S.DualSLider>
+  )
+})
+
+export { DualSlider }
