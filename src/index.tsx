00916647743b52bc0a059b64fc453efb5a6485c2
yegorkosyak <yegorkosyak32@gmail.com>
2022-12-30 21:31:46 +0100

    Revert "build: add redux toolkit"
    This reverts commit dccfb6beacdadedee8156873d51c76da9a2fe6fc.

--- src/index.tsx
+++ src/index.tsx
@@ -4,22 +4,11 @@ import './styles/index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 
-import store from './redux/store'
-import { Provider } from 'react-redux'
-
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
 root.render(
   <React.StrictMode>
-<<<<<<< Updated upstream
     <App />
-=======
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
->>>>>>> Stashed changes
   </React.StrictMode>,
 )
 
