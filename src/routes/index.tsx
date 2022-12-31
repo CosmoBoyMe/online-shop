9f3a4bee3670170d587fa28fe700ce3ac5ad85c0
CosmoboyMe <2014failz2014@gmail.com>
2022-12-31 15:20:56 +0100

    refactor: rename components files

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,7 @@
 import { FC } from 'react'
 import { Routes, Route } from 'react-router-dom'
-<<<<<<< HEAD
 import { Cart } from '../pages/Cart/Cart'
 import { Main } from '../pages/Main/Main'
-=======
-import { Main } from '../pages/Main'
->>>>>>> 8e88ef4... feat: create main page
 
 import { NotFound } from '../pages/NotFound'
 import { ProductDetails } from '../pages/ProductDetails/ProductDetails'
@@ -15,10 +11,7 @@ import { SCREENS } from './endpoints'
 const AppRoutes: FC = () => (
   <Routes>
     <Route path={SCREENS.MAIN} element={<Main />} />
-<<<<<<< HEAD
     <Route path={SCREENS.CART} element={<Cart />} />
-=======
->>>>>>> 8e88ef4... feat: create main page
     <Route path={SCREENS.NOT_FOUND} element={<NotFound />} />
   </Routes>
 )
