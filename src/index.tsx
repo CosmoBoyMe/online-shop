69ad3db1a5e1c1af3e281cfeeb0755c715e078a1
CosmoboyMe <2014failz2014@gmail.com>
2022-12-31 15:40:01 +0100

    refactor: remove dublicate import

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,6 @@ import reportWebVitals from './reportWebVitals'
 import './styles/index.css'
 import { store } from './store'
 import { Provider } from 'react-redux'
-import './styles/index.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
