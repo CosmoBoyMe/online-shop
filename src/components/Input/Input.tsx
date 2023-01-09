0c0e83b4cea380dc55a9fa4de82d82a46b5d3107
yegorkosyak <yegorkosyak32@gmail.com>
2023-01-09 23:45:26 +0100

    feat: add Input component

---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,21 @@
+import styled from 'styled-components'
+
+export const Input = styled.input`
+  border: none;
+  background-image: none;
+  background-color: transparent;
+  -webkit-box-shadow: none;
+  -moz-box-shadow: none;
+  box-shadow: none;
+
+  font-family: 'Josefin Sans';
+  font-size: 1.14286rem;
+  border-bottom: 1px solid rgb(204, 204, 204);
+  margin: 0px;
+  margin-bottom: 1rem;
+  outline: none;
+
+  &:focus {
+    outline: none;
+  }
+`
